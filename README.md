94a8d56e7337c1a5f5b9172e651479d7a33995da
JiKook31 <valdaya2013>
2019-10-11 13:39:33 +0300

    bug fixes

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 ### NER in SE
 #### Alina Ilalova
 
-**To start the training from the scratch:**
-
-1. run `download.py`
-2. run `train.py`
+**To start the training from the scratch:** run `train.py`
 
 **To run trained model on data from `input.txt`:** run `predict.py`
--- download.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-from deeppavlov import configs, build_model
-
-deeppavlov_path = f"{Path('~/.deeppavlov').expanduser()}/downloads/conll2003"
-if not os.path.isdir(deeppavlov_path):
-    ner_model = build_model(configs.ner.ner_conll2003_bert, download=True)
-    print("DOWNLOADING IS FINISHED")
